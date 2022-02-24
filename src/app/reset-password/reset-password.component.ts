d27b38df488d7ed873fc9495d5b319b1252e5aeb
Marian Francescu <marian.francescu98@gmail.com>
2022-02-24 15:37:43 +0200

    Add reset password form

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reset-password',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordComponent implements OnInit {
 
+  hideFlag: boolean = true;
+
+  resetPasswordForm = new FormGroup({
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    confirmPassword: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getPasswordErrorMessage() {
+    if (this.resetPasswordForm.controls.password.hasError('required')) {
+      return 'Please enter your password';
+    }
+    return this.resetPasswordForm.controls.password.hasError('minLength') ? '' : 'Password must have more than 6 characters';
+  }
+
+  getConfirmPasswordError() {
+    if(this.resetPasswordForm.controls.confirmPassword.hasError('required')){
+      return 'Please re-enter your password';
+    }
+    return this.resetPasswordForm.controls.password === this.resetPasswordForm.controls.confirmPassword ? 
+            '' : 'The passwords do not match'
+  }
+
+  submit() {
+    console.log('reset-password component', this.resetPasswordForm.value);
   }
 
 }
