1192fc67deffa5ec1bd797073e231b49ce564002
Marian Francescu <marian.francescu98@gmail.com>
2022-03-23 20:26:40 +0200

    Made sidenav colapsed on smaller screen

--- src/app/preferences/preferences.component.ts
+++ src/app/preferences/preferences.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 import { Router } from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Discipline } from '../models/discipline.interface';
 import { ApiService } from '../services/api.service';
 
@@ -12,10 +14,34 @@ export class PreferencesComponent implements OnInit {
   tabOption: string;
   userDisciplines: Discipline[];
   mySubscription: any;
+  mode: string;
+  openSidenav: boolean;
+  private screenWidth$ = new BehaviorSubject<number>
+    (window.innerWidth);
+
+  @ViewChild('sidenav') sidenav: MatSidenav;
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.screenWidth$.next(event.target.innerWidth);
+  }
+  getScreenWidth(): Observable<number> {
+    return this.screenWidth$.asObservable();
+  }
+
   constructor(private apiService: ApiService, private router: Router) {
   }
 
   ngOnInit(): void {
+    this.getScreenWidth().subscribe(width => {
+      if(width > 960) {
+          this.mode = 'side';
+          this.openSidenav = true;
+        }
+      if (width <= 960) {
+        this.mode = 'over';
+        this.openSidenav = false;
+      }
+    });
     this.fetchDisciplines();
   }
 
